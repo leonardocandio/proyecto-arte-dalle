e738e364d5cdca805e477a8ec0a924d208ebb3ba
leonardocandio <leonardo.candio@utec.edu.pe>
2023-11-29 23:04:00 -0500

    update stuart 2

--- src/About.js
+++ src/About.js
@@ -1,11 +1,94 @@
 import React from 'react';
+import './App.css';
 
 function About() {
-  return (
-    <div>
-      <h1>Welcome to About us</h1>
-    </div>
-  );
+    return (
+        <div className="scroll-container">
+            <div className="about-container">
+                <div>
+                    <h1>Bienvenidos</h1>
+                </div>
+                <div>
+                    <p className="home-description">
+                        Sumérgete con nosotros en un viaje fascinante que revela el impacto profundo de la sociedad en nuestro preciado medio ambiente. Descubre cómo nuestras acciones han dejado huellas en la naturaleza. ¡Acompáñanos en este emocionante recorrido por la interconexión entre la sociedad y el planeta a lo largo del tiempo! 🌍✨          </p>
+                </div>
+                <div className="imagenes-container">
+                    <div className="imagenes-container">
+                        <div className="imagen-container">
+                            <img src="https://paisajelimpio.com/wp-content/uploads/2020/07/shutterstock_1710277237-scaled-e1595410821505.jpg" alt="Antes" />
+                            <p className="home-description">
+
+                                Antes</p>
+                        </div>
+                        <div className="imagen-container">
+                            <img src="https://media.istockphoto.com/id/1208182476/es/foto/olas-empujando-residuos-plásticos-a-la-playa.jpg?s=612x612&w=0&k=20&c=nwpd2A_pGtfEV_VvLU7UhYy7LuxUwHW55EsGWJkQGfQ=" alt="Después" />
+                            <p className="home-description">
+
+                                Después</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="imagenes-container">
+                    <div className="imagenes-container">
+                        <div className="imagen-container">
+                            <img src="https://www.peru.travel/contenido/general/imagen/es/353/1.1/salcantay.jpg" alt="Antes" />
+                            <p className="home-description">
+
+                                Antes</p>
+                        </div>
+                        <div className="imagen-container">
+                            <img src="https://portal.andina.pe/EDPfotografia2/Thumbnail/2009/01/18/000084559W.jpg" alt="Después" />
+                            <p className="home-description">
+                                Después</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="imagenes-container">
+                    <div className="imagenes-container">
+                        <div className="imagen-container">
+                            <img src="https://cnnespanol.cnn.com/wp-content/uploads/2023/09/rio-amazonas.jpg?quality=100&strip=info&w=940&h=530&crop=1" alt="Antes" />
+                            <p className="home-description">
+                                Antes</p>
+                        </div>
+                        <div className="imagen-container">
+                            <img src="https://media-cdn.tripadvisor.com/media/photo-s/01/1b/e6/90/lecho-de-rio-seco.jpg" alt="Después" />
+                            <p className="home-description">
+
+                                Después</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="imagenes-container">
+                    <div className="imagenes-container">
+                        <div className="imagen-container">
+                            <img src="https://cdn.unenvironment.org/decadeonrestoration/2020-03/nature-3294681_1280%20%281%29.jpg" alt="Antes" />
+                            <p className="home-description">
+                                Antes</p>
+                        </div>
+                        <div className="imagen-container">
+                            <img src="https://e.rpp-noticias.io/xlarge/2016/06/27/500750_178827.jpg" alt="Después" />
+                            <p className="home-description">
+                                Después</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="imagenes-container">
+                    <div className="imagenes-container">
+                        <div className="imagen-container">
+                            <img src="https://www.telemundo.com/sites/nbcutelemundo/files/styles/fit-1240w/public/images/article/cover/2019/01/07/oceano-pacifico.jpg?ramen_itok=iqwQftIcTf" alt="Antes" />
+                            <p className="home-description">
+                                Antes</p>
+                        </div>
+                        <div className="imagen-container">
+                            <img src="https://cnnespanol.cnn.com/wp-content/uploads/2020/08/01.jpg" alt="Después" />
+                            <p className="home-description">
+                                Después</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default About;
