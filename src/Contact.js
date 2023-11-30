94a1723bf8df71fff07f0f526fae6fb3208e8c8a
leonardocandio <leonardo.candio@utec.edu.pe>
2023-11-29 23:03:43 -0500

    update stuart

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,28 @@
+// Contact.js
 import React from 'react';
+import UTEC from './UTEC-Logo-removebg-preview.png';
+
+import './Contact.css'; // Archivo de estilos
 
 function Contact() {
   return (
-    <div>
-      <h1>Welcome to Contact us</h1>
+    <div className="contact-container">
+      {/* Imagen UTEC a la izquierda */}
+      <img
+        src={UTEC}
+        alt="UTEC Logo"
+        style={{ width: '38%', maxWidth: '1000px', marginRight: '20px' }}
+      />
+
+      {/* Texto y cuadro a la derecha */}
+      <div className="text-container">
+        <h1>Contáctanos</h1>
+        <div className="text-box">
+        <p className="home-description">
+  Únete a nosotros, estudiantes de la universidad UTEC en la Facultad de Computación, apasionados por marcar la diferencia. Estamos comprometidos a concientizar sobre el daño ambiental y buscamos tu apoyo. Comunícate con nosotros en <strong>ximena.lindo@utec.edu.pe</strong> para ser parte de esta iniciativa positiva.
+</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Service.js
+++ src/Service.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import DALLE from './OpenAI-DALL-E.png';
+import './App.css';
 
 function Service() {
   return (
     <div>
       <h1>Welcome to Service</h1>
-    </div>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <img
+          src={DALLE}
+          alt="Imagen Grande"
+          style={{ width: '80%', maxWidth: '600px', height: 'auto' }}
+        />
+      </div>
+      <p className="home-description">
+  Simulamos la degradación del ambiente usando  <strong>IA</strong> proporcionada por Dalle 2, el usuario ingresa una imagen
+  y puede visualizar como sería una degradación del ambiente al pasar de los años.
+</p>    </div>
   );
 }
 
